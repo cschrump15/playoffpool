93fda185765be70c4e589a9f73416374e66deacb
cschrump15 <cschrump15@gmail.com>
2026-03-01 16:08:06 -0500

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,7 @@
-export const metadata = {
-  title: '2026 Playoff Pool',
-  description: 'Chris NHL NBA Playoff Pool',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{margin: 0, padding: 0, background: '#0a0e1a'}}>
+      <body>
         {children}
       </body>
     </html>
