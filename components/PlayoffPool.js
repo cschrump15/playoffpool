d8ac83d3adf3d2dcca77914999617392c8893971
cschrump15 <cschrump15@gmail.com>
2026-05-18 18:31:26 -0400

    Update PlayoffPool.js

--- components/PlayoffPool.js
+++ components/PlayoffPool.js
@@ -281,7 +281,7 @@ export default function PlayoffPool() {
 
   async function loadParticipants() {
     const { data: users } = await supabase.from('users').select('*')
-    const { data: picks } = await supabase.from('picks').select('*')
+    const { data: picks } = await supabase.from('picks').select('*').range(0, 9999)
     const { data: allSeries } = await supabase.from('series').select('*')
     if (users && picks && allSeries) {
       setAllPicks(picks)
